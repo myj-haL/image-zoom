12bfed8c81624dc60873842aefaa3076486b99ad
myj <myj262@gmail.com>
2025-01-24 10:20:29 +0900

    works mobile

--- src/app/components/SectionTitle/index.jsx
+++ src/app/components/SectionTitle/index.jsx
@@ -1,8 +1,8 @@
 import styles from "./index.module.css";
 
-export default function sectionTitle ({subTitle, title}) {
+export default function sectionTitle ({subTitle, title, option}) {
   return (
-    <div className={styles.wrap}>
+    <div className={`${styles.wrap} ${option && styles.white }`}>
       <span className={styles.sub_title}>{subTitle}</span>
       <h3 className={styles.title}>{title}</h3>
     </div>
--- src/app/main/works/index.jsx
+++ src/app/main/works/index.jsx
@@ -1,21 +1,63 @@
 import styles from "./index.module.css";
 import worksList from "./worksList";
 import SectionTitle from "@/app/components/SectionTitle";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 
 export default function Works () {
+  const chunkArray = (array, size) => {
+    const chunked = [];
+    for (let i = 0; i < array.length; i += size) {
+      chunked.push(array.slice(i, i + size));
+    }
+    return chunked;
+  };
+  
+  // Create chunks with 3 items per group
+  const groupedWorksList = chunkArray(worksList, 3);
+  
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
         <SectionTitle 
           title="Works"
           subTitle="Company"
+          option='white'
         />
 
-        <div className={styles.works_list}>
-          {worksList.map((item, idx) => (
-            <div className={styles.works_item} key={item.id}></div>
-          ))}
-        </div>
+        
+        <Swiper className={styles.works_list} 
+          pagination={true} 
+          modules={[Pagination]} 
+          slidesPerView={1} 
+          spaceBetween={30}
+          >
+            {groupedWorksList.map((group, groupIdx) => (
+              <SwiperSlide className={styles.works_item} key={groupIdx}>
+                {group.map((item, idx) => (
+                  <div className={styles.group} key={item.id}>
+                    <div className={styles.group_left}>
+                      <h3 className={styles.name}>
+                        {item.company}
+                        <p>{item.date}</p>
+                      </h3>
+                      <span className={styles.position}>{item.position}</span>
+                    </div>
+
+                    <ul className={styles.group_right}>
+                      {item.role.map((item2) => (
+                        <li key={item2.id}>
+                          {item2.content}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </SwiperSlide>
+            ))}
+        </Swiper>
       </div>
     </div>
   )
