618b9ee12dcaf436be33e50cf6e00468395ff1dd
myj <myj262@gmail.com>
2025-02-16 15:32:59 +0900

    webpack 수정

--- next.config.mjs
+++ next.config.mjs
@@ -8,20 +8,25 @@ const nextConfig = {
   ? "https://myj-hal.github.io/image-zoom/"
   : "",
   webpack(config) {
-    // css-loader의 CSS Modules 옵션에서 pure 모드를 비활성화
-    config.module.rules
-      .filter((rule) => Array.isArray(rule.oneOf))
-      .flatMap((rule) => rule.oneOf)
-      .forEach((one) => {
-        one.use?.forEach((loader) => {
-          if (
-            loader.loader?.includes("css-loader") &&
-            loader.options?.modules
-          ) {
-            loader.options.modules.pure = false;
-          }
-        });
+    // 재귀적으로 모든 규칙을 순회하는 함수
+    const updateCssLoader = (rules) => {
+      rules.forEach((rule) => {
+        if (rule.use) {
+          const loaders = Array.isArray(rule.use) ? rule.use : [rule.use];
+          loaders.forEach((loader) => {
+            if (loader.options && loader.options.modules) {
+              // pure 모드를 비활성화합니다.
+              loader.options.modules.pure = false;
+            }
+          });
+        }
+        if (rule.oneOf) {
+          updateCssLoader(rule.oneOf);
+        }
       });
+    };
+
+    updateCssLoader(config.module.rules);
     return config;
   },
 };
