0ffe64db22cc44274bd37ff35e94a4c2d7051d57
myj <myj262@gmail.com>
2025-02-16 15:06:49 +0900

    배포 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production"
+  ? "https://myj-hal.github.io/image-zoom/"
+  : "",
+};
 
 export default nextConfig;
